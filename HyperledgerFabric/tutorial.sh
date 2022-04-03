855c54c73cb4e716916e623363b051b26c2afb86
Trey Manuszak <manuszaktrey@gmail.com>
2022-04-02 22:45:52 -0700

    working on tutorial

---
+++ HyperledgerFabric/tutorial.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install golang-go
+sudo apt install nodejs
+
+# Install Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Make sure the daemon is running
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -a -G docker root
+
+# Install Hyperledger Fabric samples, binaries, and Docker images
+curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.2.2 1.4.9
+export PATH=~/fabric-samples/bin:$PATH
+
+# Tutorials - Using the Fabric test network
+cd fabric-samples/test-network
+./network.sh down
+./network.sh up
+./network.sh createChannel
+
+# Starting a chaincode on the channel
+# BEFORE RUNNING THE NEXT COMMAND! Edit asset-transfer-basic/chaincode-go/go.mod from "go 1.14" to "go 1.13"
+./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-go -ccl go
